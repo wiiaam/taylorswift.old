d098abdf8e6057b316623346307bff6ffa3e84ab
wiiam <wiiam24@gmail.com>
2015-06-18 13:55:46 +1200

    Added the ability to turn on and off

--- modules/KiwiIRC.java
+++ modules/KiwiIRC.java
@@ -3,9 +3,18 @@ package modules;
 import bot.Message;
 
 public class KiwiIRC implements Module {
-
+	
+	private boolean on = false;
+	
 	@Override
 	public void parse(Message m) {
+		if(m.command().equalsIgnoreCase("kickkiwiirc") && m.admins.contains(m.sender())){
+			on = true;
+		}
+		if(m.command().equalsIgnoreCase("allowkiwiirc") && m.admins.contains(m.sender())){
+			on = false;
+		}
+		if(!on) return;
 		if(m.command().equals("JOIN")){
 			if(m.senderWhole().contains("KiwiIRC")){
 				m.send(String.format("KICK %s %s :KiwiIRC", m.trailing(), m.sender()));
