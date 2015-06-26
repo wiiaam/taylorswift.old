f0edc2eaedb29bfdbb155be857407cd41059227a
William <wiiam24@gmail.com>
2015-06-27 01:42:04 +1200

    changed to not kick users

--- modules/KiwiIRC.java
+++ modules/KiwiIRC.java
@@ -8,16 +8,16 @@ public class KiwiIRC implements Module {
 	
 	@Override
 	public void parse(Message m) {
-		if(m.command().equalsIgnoreCase("kickkiwiirc") && m.admins.contains(m.sender())){
+		if(m.botCommand().equalsIgnoreCase("kiwion") && m.admins.contains(m.sender())){
 			on = true;
 		}
-		if(m.command().equalsIgnoreCase("allowkiwiirc") && m.admins.contains(m.sender())){
+		if(m.botCommand().equalsIgnoreCase("kiwioff") && m.admins.contains(m.sender())){
 			on = false;
 		}
 		if(!on) return;
 		if(m.command().equals("JOIN")){
-			if(m.senderWhole().contains("KiwiIRC")){
-				m.send(String.format("KICK %s %s :KiwiIRC", m.trailing(), m.sender()));
+			if(m.senderWhole().toLowerCase().contains("kiwiirc")){
+				m.pm(m.trailing(), m.sender() + ": get a real client faggot");
 			}
 		}
 
