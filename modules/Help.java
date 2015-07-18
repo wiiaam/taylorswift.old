d6188072092133ecb8cf805d59260b70d52aa48a
wiiam <wiiam24@gmail.com>
2015-07-18 15:44:53 +1200

    change to /invite

--- modules/Help.java
+++ modules/Help.java
@@ -1,26 +1,30 @@
 package modules;
 
 import bot.Message;
+import bot.config.Config;
 
 public class Help implements Module {
 
 	@Override
 	public void parse(Message m) {
-		if(m.trailing().startsWith(".help") || m.botCommand().equals("help")){
+		String target = m.param();
+		if(!m.param().startsWith("#")) target = m.sender();
+		if(m.trailing().startsWith(".help") || m.botCommand().equals("help") || (m.param().equals(Config.getNick()) && m.trailing().toLowerCase().equals("help"))){
+			/*
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
-			}
+			}*/
 			m.notice(m.sender(), "Commands can be found at https://github.com/wiiam/Personal-Bot/blob/master/COMMANDS.md");
 			m.notice(m.sender(), "Source and license info can be found with .source and .license respectively");
 			m.notice(m.sender(), "My commandchar is currently " + m.commandChar());
-			m.notice(m.sender(), "If you want me to join a channel, join #taylorswift and make a request");
+			m.notice(m.sender(), "If you want me to join a channel, use /invite " + Config.getNick());
 		}
-		if(m.trailing().startsWith(".source") || m.botCommand().equals("source")){
-			m.notice(m.sender(), "https://github.com/wiiam/Personal-Bot");
+		if(m.trailing().startsWith(".source") || m.botCommand().equals("source") || (m.param().equals(Config.getNick()) && m.trailing().toLowerCase().equals("source"))){
+			m.say(m.sender(), "https://github.com/wiiam/Personal-Bot");
 		}
-		if(m.trailing().startsWith(".license") || m.botCommand().equals("license")){
+		if(m.trailing().startsWith(".license") || m.botCommand().equals("license") || (m.param().equals(Config.getNick()) && m.trailing().toLowerCase().equals("license"))){
 			m.notice(m.sender(), "I am licensed under GNU AGPL. The terms of this license can be found here: https://github.com/wiiam/Personal-Bot/blob/master/LICENSE");
 		}
 
