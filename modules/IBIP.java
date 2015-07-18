593d42f7e136b489dedc22102824bc2b425070d2
wiiam <wiiam24@gmail.com>
2015-07-18 15:45:11 +1200

    only respond to .bots

--- modules/IBIP.java
+++ modules/IBIP.java
@@ -9,7 +9,7 @@ public class IBIP implements Module {
 	
 	@Override
 	public void parse(Message m) {
-		if(m.trailing().startsWith(".bots")){
+		if(m.trailing().split("\\s+")[0].equals(".bots")){
 			String target = m.param();
 			if(!m.param().startsWith("#")) target = m.sender();
 			m.say(target, String.format("Reporting in! [Java] %s", Lyrics.getRandomShortLyric()));
