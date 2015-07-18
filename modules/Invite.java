bf01b321b590bc37dcf4ef58aac3e7d2001efcd0
wiiam <wiiam24@gmail.com>
2015-07-18 15:45:30 +1200

    allow users to /invite the bot to other channels

---
+++ modules/Invite.java
@@ -0,0 +1,18 @@
+package modules;
+
+import bot.Message;
+import bot.config.Config;
+
+public class Invite implements Module {
+
+	@Override
+	public void parse(Message m) {
+		if(m.command().equals("INVITE")){
+			m.send("JOIN " + m.trailing());
+			Config.addRoom(m.trailing());
+			m.say(m.trailing(), m.sender() + " has invited me here. To see my commands, use /MSG " + Config.getNick() + " HELP");
+		}
+
+	}
+
+}
