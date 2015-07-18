ac0cada727928fcda10ed4987be0cd74feccfce1
wiiam <wiiam24@gmail.com>
2015-07-18 13:42:33 +1200

    clean

--- modules/Ignores.java
+++ modules/Ignores.java
@@ -9,20 +9,20 @@ public class Ignores implements Module {
 		String target = m.param();
 		if(!m.param().startsWith("#")) target = m.sender();
 		if(Config.getAdmins().contains(m.sender())){
-			if(m.botCommand().equals("ignores")){
-				if(m.botParamsArray().length > 1){
-					if(m.botParamsArray()[0].equals("add")){
-						Config.addIgnore(m.botParamsArray()[1]);
-						m.say(target, m.botParamsArray()[1] + " is now being ignored.");
+			if(m.hasBotParams()){
+				if(m.botCommand().equals("ignore")){				
+					for(String nick : m.botParamsArray()){
+						Config.addIgnore(nick);
 					}
-					if(m.botParamsArray()[0].equals("del")){
-						if(Config.removeIgnore(m.botParamsArray()[1])) m.say(target, m.botParamsArray()[1] + " is no longer being ignored");
-						else m.say(target, m.botParamsArray()[1] + " was never being ignored");
+					m.say(target, "The specified users have been ignored");
+				}
+				if(m.botCommand().endsWith("unignore")){
+					for(String nick : m.botParamsArray()){
+						Config.removeIgnore(nick);
 					}
+					m.say(target, "The specified users have been unignored");
 				}
 			}
 		}
-
 	}
-
 }
