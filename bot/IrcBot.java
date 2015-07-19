5c1c510b24b509944c0513efdafd6947bb3414b4
wiiam <wiiam24@gmail.com>
2015-07-19 16:37:10 +1200

    change IrcBot to static

--- bot/IrcBot.java
+++ bot/IrcBot.java
@@ -14,12 +14,10 @@ public class IrcBot {
 	// Constants
 	
 	// Fields
-	private HashSet<Module> modules;
-	private Server server;
-	private PrintStream out = System.out;
+	public static HashSet<Module> modules;
+	private static PrintStream out = System.out;
 	
-	
-	public IrcBot(){
+	public static void start(){
 		out.println("Reading config");
 			try {
 				readConfig();
@@ -29,10 +27,9 @@ public class IrcBot {
 			}
 			
 		out.println("Loading modules");
-		loadModules();
-		out.println("Connecting to server");
-		server = new Server();
-		server.connectTo(Config.getServer(), Config.getPort());
+		modules = Modules.loadAll();
+		out.println("Connecting to Server");
+		Server.connectTo(Config.getServer(), Config.getPort());
 		
 		out.println("Logging in");
 		if(attemptLogin()){
@@ -42,72 +39,36 @@ public class IrcBot {
 		}
 	}
 	
-	public void loadModules(){
-		modules = new HashSet<Module>();
-		
-		modules.add(new IBIP());
-		
-		modules.add(new Admin());
-		//modules.add(new Autojoin());
-		modules.add(new Cowsay());
-		modules.add(new Fortune());
-		modules.add(new Google());
-		modules.add(new Help());
-		modules.add(new Ignores());
-		modules.add(new Invite());
-		modules.add(new KiwiIRC());
-		modules.add(new London());
-		modules.add(new Money());
-		modules.add(new NoBro());
-		modules.add(new Ping());
-		modules.add(new Quotes());
-		modules.add(new Rainbow());
-		modules.add(new Random());
-		modules.add(new SongLyrics());
-		modules.add(new Spam());
-		modules.add(new Steam());
-		modules.add(new Time());
-		modules.add(new TopKek());
-		modules.add(new Triggers());
-		modules.add(new TitleReporting());
-		modules.add(new UrbanDictionary());
-		modules.add(new modules.UserInfo());
-		modules.add(new Version());
-		modules.add(new Voting());
-		modules.add(new Youtube());
-		modules.add(new Weather());
-		
-	}
 	
-	public void sendOnLogin(){
-		server.send(Config.getIdentification());
+	public static void sendOnLogin(){
+		Server.send(Config.getIdentification());
 		try {
 			Thread.sleep(2000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		for(String s : Config.getRooms()){
-			server.send("JOIN #" + s);
-			server.send("WHO #" + s);
+			Server.send("JOIN #" + s);
+			Server.send("WHO #" + s);
 		}
 	}
 	
-	public boolean readConfig() throws FileNotFoundException, URISyntaxException{
-		Config.load(new File(this.getClass().getResource("config/config.json").toURI()));
+	public static boolean readConfig() throws FileNotFoundException, URISyntaxException{
+		Config.load(new File(new IrcBot().getClass().getResource("config/config.json").toURI()));
 		return true;
 	}
 	
 	/**
-	 * Attempts to login to the server
+	 * Attempts to login to the Server
 	 */
-	public boolean attemptLogin(){
-		server.send("NICK " + Config.getNick());
-		server.send(String.format("USER %s %s %s :%s", Config.getUsername(), Config.getUsername(), Config.getServer(), Config.getRealname()));
+	public static boolean attemptLogin(){
+		Server.send("NICK " + Config.getNick());
+		Server.send(String.format("USER %s %s %s :%s", Config.getUsername(), Config.getUsername(), Config.getServer(), Config.getRealname()));
 		while(true){
-			if(server.in.hasNextLine()){
-				String next = server.in.nextLine();
+			if(Server.in.hasNextLine()){
+				String next = Server.in.nextLine();
 				System.out.println(next);
-				Message m = new Message(next, server);
+				Message m = new Message(next);
 				if(m.command().equals("001")) break;
 			}
 		}
@@ -115,12 +76,12 @@ public class IrcBot {
 		
 	}
 	
-	public void listenToServer() {
+	public static void listenToServer() {
 		new Thread(new Runnable(){
 			public void run() {
 				while(true){
-				if(server.in.hasNextLine()){
-					final Message m = new Message(server.in.nextLine(), server);
+				if(Server.in.hasNextLine()){
+					final Message m = new Message(Server.in.nextLine());
 					if(!Config.getAdmins().contains(m.sender()) && Config.getIgnores().contains(m.sender())) continue;
 					out.println(m.message());
 					for(Module module : modules){
