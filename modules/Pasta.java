7a9e19067107248217af831298ab28bba4ad2c95
wiiam <wiiam24@gmail.com>
2015-07-24 02:19:55 +1200

    change motd

--- modules/Pasta.java
+++ modules/Pasta.java
@@ -9,7 +9,7 @@ public class Pasta implements Module {
 	public void parse(Message m) {
 		if(m.command().equals("JOIN") && m.trailing().equals("#pasta")){
 			m.notice(m.sender(), "2Welcome to 4#pasta2. Please check the rules with ~rules, unless cuck is ded.");
-			m.notice(m.sender(), "2gen2 is currently ded as of 19th July due to cuck related activities");
+			//m.notice(m.sender(), "2gen2 is currently ded as of 19th July due to cuck related activities");
 		}
 	}
 
